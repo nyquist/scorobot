431c67334124d9482df6b0dd847dc72214ead708
nyquist <nyquist@vaspun.eu>
2020-04-29 01:31:17 +0300

    verbose intial output

--- globalcfg.py
+++ globalcfg.py
@@ -5,7 +5,8 @@ from utils.backend_SQLite import SQLiteDB
 
 load_dotenv("./storage/config/.env")
 backend_location = "./storage/games.db"
-print(Path(backend_location).absolute())
+print("Using backend: ", Path(backend_location).absolute())
+print("Using token: ", os.getenv('DISCORD_TOKEN'))
 backend = SQLiteDB(backend_location)
 token = os.getenv('DISCORD_TOKEN')
 
