be3f6d8946e72a0e262db9d7422db16eb61e5d51
Nyquist <nyquist@vaspun.eu>
2020-05-17 11:43:24 +0300

    refactor reactions with generics

--- platforms/botplatform.py
+++ platforms/botplatform.py
@@ -30,51 +30,93 @@ class BotPlatform:
         self.lastCancelled = None
         self.toBeConfirmed = None
         self.testing = testing
+        self.reactions = {
+        'result': {
+            'is': self._isResult,
+            'do': lambda *a: '"{}"-"{}": "{}" - "{}" ? Please confirm'.format(self.toBeConfirmed.teamA, self.toBeConfirmed.teamB, self.toBeConfirmed.scoreA, self.toBeConfirmed.scoreB)
+            },
+        'yes': {
+            'is': lambda m: self.genericReaction(m, r"^(yes|y)$",r"^(ok)$"),
+            'do': lambda *a: 'Added #{}: **{}**-**{}**: {}-{}'.format(self.lastValid.gameId,self.lastValid.teamA, self.lastValid.teamB, self.lastValid.scoreA, self.lastValid.scoreB, )
+            },
+        'no': {
+            'is': lambda m: self.genericReaction(m, r"^(no?)$", r"^(cancel)$"),
+            'do': lambda *a: 'Canceled: "{}"-"{}": "{}" - "{}". ({})'.format(self.lastCancelled.teamA, self.lastCancelled.teamB, self.lastCancelled.scoreA, self.lastCancelled.scoreB, a[0])
+            },
+        'status': {
+            'is': lambda m: self.genericReaction(m, r"^status$"),
+            'do': lambda *a: f"""
+```
+lastValid = {self.lastValid} 
+lastCancelled = {self.lastCancelled}
+toBeConfirmed = {self.toBeConfirmed}
+TTL = {None if self.toBeConfirmed is None else self.toBeConfirmed.TTL}
+testing = {self.testing}
+running_on = {platform.system()}-{platform.release()}
+```
+"""
+            },
+        'all-time': {
+            'is': lambda m: self.genericReaction(m, r"^all-time$"),
+            'do': lambda *a: self.ranking()
+            },
+        'today': {
+            'is': lambda m: self.genericReaction(m, r"^today$"),
+            'do': lambda *a: self.ranking(24)
+            },
+        'games': {
+            'is': lambda m: self.genericReaction(m, r"^games$"),
+            'do': lambda *a: self.games(24)
+            },
+        'help': {
+            'is': lambda m: self.genericReaction(m, r"^help$"), 
+            'do': lambda *a: """
+`teamA - teamB X-Y | teamA teamB X-Y | X-Y`: Register game
+`ok | yes | y`: Confirm result
+`cancel | no | n`:Cancel result
+`all-time`: All time standings
+`today`: Today's standings
+`games`: Today's games
+`help`: This message
+`status`: Status"""
+            },
+    }
 
         
     def parseMessage(self,message_content):
         """
         returns the answer to bot
         """
-
-        if self._isResult(message_content):
-            return self._onResult()
-        elif self._isConfirmation(message_content):
-            self.toBeConfirmed.confirm()
-            self.lastValid = self.toBeConfirmed
-            self.toBeConfirmed = None
-            return self._onConfirmation()
-        elif self._isCancellation(message_content):
-            self.lastCancelled = self.toBeConfirmed
-            self.toBeConfirmed = None
-            return self._onCancellation("User cancelled")
-        elif self._isStatus(message_content):
-            return self._onStatus()
-        elif self._isAllTime(message_content):
-            return self._onAllTime()
-        elif self._isGames(message_content):
-            return self._onGames()
-        elif self._isToday(message_content):
-            return self._onToday()
-        elif self._isHelp(message_content):
-            return self._onHelp()
-
+        for (key, r) in self.reactions.items():
+        ## Go through all registere reactions:
+            if r['is'](message_content):
+                # If I should react: (is)
+                if key == 'yes':
+                    #Special case for yes
+                    self.toBeConfirmed.confirm()
+                    self.lastValid = self.toBeConfirmed
+                    self.toBeConfirmed = None
+                if key == 'no':
+                    #Special case for no
+                    self.lastCancelled = self.toBeConfirmed
+                    self.toBeConfirmed = None
+                    return r['do']('User cancelled')
+                #Applies to any other reaction:
+                return r['do']()
+        # TTL for confirmations (in case none of the other reactions apply):
         self.toBeConfirmed.TTL = self.toBeConfirmed.TTL - 1
         if self.toBeConfirmed.TTL == 0:
             self.lastCancelled = self.toBeConfirmed
             self.toBeConfirmed = None
-            return self._onCancellation("No confirmation received.")
+            return self.reactions['no']['do']('No confirmation received')
         return None
     
 
 
 
-#### Reactions.
+#### Reactions
 
-    
-
-
-    def genericReaction(self, message, list_of_triggers):
+    def genericReaction(self, message, *list_of_triggers):
         for t in list_of_triggers: 
             search_result = re.search(t, message.lower())
             if search_result is not None:
@@ -82,12 +124,13 @@ class BotPlatform:
         return False
 
     
+    
     def _isResult(self,message):
         results = [
             #TeamA - TeamB 4-2
-            r"^\s*(\S+)\s*[ -]+\s*(\S+)\s*(\d+)[ -]+(\d+)",
+            r"^\s*(\S+)[\s-]+(\S+)\s*(\d+)[\s-]+(\d+)",
             #3 - 2. Should assume the previous teams
-            r"^(\d+)\s*[- ]\s*(\d+)$",
+            r"^(\d+)[-\s]+(\d+)$",
         ]
         for r in results:
             search_result = re.search(r,message)
@@ -101,110 +144,25 @@ class BotPlatform:
                     self.toBeConfirmed = Validator(x[0].strip(), x[1].strip(), int(x[2]), int(x[3]), self.tournament)
                     return True
         return False
-    def _onResult(self):
-        return 'Added #{}: **{}**-**{}**: {}-{}'.format(self.lastValid.gameId,self.lastValid.teamA, self.lastValid.teamB, self.lastValid.scoreA, self.lastValid.scoreB, )
 
-    def _isConfirmation(self, message):
-        triggers = [
-            # y, yes
-            r"^(yes|y)$",
-            r"^(ok)$"
-        ]
-        return self.genericReaction(message, triggers)
-
-    def _onConfirmation(self):
-        return '"{}"-"{}": "{}" - "{}" ? Please confirm'.format(self.toBeConfirmed.teamA, self.toBeConfirmed.teamB, self.toBeConfirmed.scoreA, self.toBeConfirmed.scoreB)
-
-    def _isCancellation(self, message):
-        triggers = [
-            # n, no
-            r"^(no?)$",
-            #cancel
-            r"^(cancel)$"
-        ]
-        return self.genericReaction(message, triggers)
-
-    def _onCancellation(self, reason = ""):
-        return 'Canceled: "{}"-"{}": "{}" - "{}". ({})'.format(self.lastCancelled.teamA, self.lastCancelled.teamB, self.lastCancelled.scoreA, self.lastCancelled.scoreB, reason)
-
-    def _isStatus(self, message):
-        triggers = [
-            # status
-            r"^status$",
-        ]
-        return self.genericReaction(message, triggers)
-
-    def _onStatus(self):
-        return f"""
-```
-lastValid = {self.lastValid} 
-lastCancelled = {self.lastCancelled}
-toBeConfirmed = {self.toBeConfirmed}
-testing = {self.testing}
-running_on = {platform.system()}-{platform.release()}
-```
-"""
-
-    def _isAllTime(self, message):
-        triggers = [
-            #clasament
-            r"^all-time$",
-        ]
-        return self.genericReaction(message, triggers)
-
-    def _onAllTime(self):
-        response = "```"
-        response = response + "\n{T:10} {M:>2} {W:>2} {D:>2} {L:>2} {GF:>3} {GA:>3} {P:>3}".format(T="Team",M="M", W="W", D="D", L="L", GF="GF", GA="GA", P="P")
-        for line in self.tournament.getRanking():
-            response = response +  "\n{T:10} {M:2} {W:2} {D:2} {L:2} {GF:3} {GA:3} {P:3}".format(T=line[0],M=line[1]['w']+line[1]['d']+line[1]['l'], W=line[1]['w'], D=line[1]['d'], L=line[1]['l'], GF=line[1]['gf'], GA=line[1]['ga'], P=line[1]['p'])
-        response = response + "\n```"
-        return response
-
-    def _isGames(self, message):
-        triggers = [
-            #games
-            r"^games$",
-        ]
-        return self.genericReaction(message, triggers)
+    def ranking(self, hours = None):
+        
+        if hours is None:
+            standings = self.tournament.getRanking()
+        else:
+            standings = self.tournament.getRanking(24)
+        response ="\n{T:10} {M:>2} {W:>2} {D:>2} {L:>2} {GF:>3} {GA:>3} {P:>3}".format(T="Team",M="M", W="W", D="D", L="L", GF="GF", GA="GA", P="P")
+        for line in standings:
+            response +=  "\n{T:10} {M:2} {W:2} {D:2} {L:2} {GF:3} {GA:3} {P:3}".format(T=line[0],M=line[1]['w']+line[1]['d']+line[1]['l'], W=line[1]['w'], D=line[1]['d'], L=line[1]['l'], GF=line[1]['gf'], GA=line[1]['ga'], P=line[1]['p'])
+        return f"```{response}```"
     
-    def _onGames(self):
-        response = "```"
-        for line in self.tournament.getGames(24):
-            response = response +  "\n{ID:4}. {D:10}:  {T1:>10} {S1} - {S2} {T2:<10}".format(ID=line[0],D=time.ctime(float(line[1])), T1=line[2], T2=line[3], S1=line[4], S2=line[5])
-        response = response + "\n```"
-        return response
+    def games(self, hours = None):
+        if hours is None:
+            games = self.tournament.getGames()
+        else:
+            games = self.tournament.getGames(24)
+        response = ''
+        for line in games:
+            response = "\n{ID:4}. {D:10}:  {T1:>10} {S1} - {S2} {T2:<10}".format(ID=line[0],D=time.ctime(float(line[1])), T1=line[2], T2=line[3], S1=line[4], S2=line[5])
+        return f"```{response}```"
 
-    def _isToday(self, message):
-        triggers = [
-            #games
-            r"^today$",
-        ]
-        return self.genericReaction(message, triggers)
-
-    def _onToday(self):
-        response = "```"
-        response = response + "\n{T:10} {M:>2} {W:>2} {D:>2} {L:>2} {GF:>3} {GA:>3} {P:>3}".format(T="Team",M="M", W="W", D="D", L="L", GF="GF", GA="GA", P="P")
-        for line in self.tournament.getRanking(24):
-            response = response +  "\n{T:10} {M:2} {W:2} {D:2} {L:2} {GF:3} {GA:3} {P:3}".format(T=line[0],M=line[1]['w']+line[1]['d']+line[1]['l'], W=line[1]['w'], D=line[1]['d'], L=line[1]['l'], GF=line[1]['gf'], GA=line[1]['ga'], P=line[1]['p'])
-        response = response + "\n```"
-        return response
-
-    def _isHelp(self, message):
-        triggers = [
-            #games
-            r"^help$",
-        ]
-        return self.genericReaction(message, triggers)
-
-
-    def _onHelp(self):
-        response = """
-`teamA - teamB X-Y | teamA teamB X-Y | X-Y`: Register game
-`ok | yes | y`: Confirm result
-`cancel | no | n`:Cancel result
-`all-time`: All time standings
-`today`: Today's standings
-`games`: Today's games
-`help`: This message
-`status`: Status"""
-        return response
